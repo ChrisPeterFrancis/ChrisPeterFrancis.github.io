0c4bcd8148ebbeffa9e2f25ac4f76de2aad696a7
ChrisPeterFrancis <attacklions@gmail.com>
2017-02-27 11:27:33 -0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,6 @@
+$(function(){
+	$(".Contents").typed({
+		strings: ['email &nbsp; : <a href="mailto:hello@aditkamath.com">hello@aditkamath.com</a> ^500 <br> twitter : <a href="https://twitter.com/aditkamath">@aditkamath</a> ^500 <br> github &nbsp;: <a href="https://github.com/aditkamath">@aditkamath</a>'],
+		showCursor: false
+	});
+});
